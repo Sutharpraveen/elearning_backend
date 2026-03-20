5551ff6f678c5f84890f84ca0b9ba34bb366c995
Praveen@52121 <sutharpraveen234@gmail.com>
2026-03-20 23:22:29 +0530

    feat: add venv/bin to PATH in wsgi for static ffmpeg binary support

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add venv bin to PATH so ffmpeg can be found when running under mod_wsgi
+venv_bin = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'venv', 'bin')
+if venv_bin not in os.environ.get('PATH', ''):
+    os.environ['PATH'] = f"{venv_bin}:{os.environ.get('PATH', '')}"
 
 from django.core.wsgi import get_wsgi_application
-                                                                                                                                                                                                                                                                                                                    
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
 
 application = get_wsgi_application()
